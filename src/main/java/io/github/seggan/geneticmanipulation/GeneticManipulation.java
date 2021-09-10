f2d25e1a5178c98e2b825979d579ab2e80f59d15
Seggan <segganew@gmail.com>
2021-09-10 16:40:33 -0400

    Change it to my needs

---
+++ src/main/java/io/github/seggan/geneticmanipulation/GeneticManipulation.java
@@ -0,0 +1,20 @@
+package io.github.seggan.geneticmanipulation;
+
+import io.github.mooy1.infinitylib.core.AbstractAddon;
+
+public class GeneticManipulation extends AbstractAddon {
+
+    public GeneticManipulation() {
+        super("Seggan", "GeneticManipulation", "master", "auto-update");
+    }
+
+    @Override
+    protected void enable() {
+
+    }
+
+    @Override
+    protected void disable() {
+
+    }
+}
